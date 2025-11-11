dae8e2749272cb2614b5b142a4889c0f909d2b5f
zac wang <360188606@qq.com>
2025-11-11 10:42:36 +0800

    Update 01_deploy_MyAuction.js

--- deploy/01_deploy_MyAuction.js
+++ deploy/01_deploy_MyAuction.js
@@ -6,10 +6,10 @@ module.exports = async ({getNamedAccounts, deployments}) => {
   const {deploy} = deployments;
   const {deployer} = await getNamedAccounts();
   
-  console.log("开始部署MyAuction合约...");
+  console.log("开始部署MyAuction合约 beacon...");
   const MyAuction = await ethers.getContractFactory("MyAuction");
-  const MyAuctionImpl = await upgrades.deployImplementation(MyAuction);
-  console.log("部署完成MyAuction合约 :", MyAuctionImpl);
+  const beacon = await upgrades.deployBeacon(MyAuction);
+  console.log("部署完成MyAuction合约 beacon :", beacon);
 
   console.log("开始部署PriceProvider合约...");
   const PriceProvider = await ethers.getContractFactory("PriceProvider");
@@ -19,7 +19,7 @@ module.exports = async ({getNamedAccounts, deployments}) => {
 
   console.log("开始部署MyAuctionFactory合约...");
   const MyAuctionFactory = await ethers.getContractFactory("MyAuctionFactory");
-  const myAuctionFactoryProxy = await upgrades.deployProxy(MyAuctionFactory, [getAddr(deployer), getAddr(priceProvider), getAddr(MyAuctionImpl)], {
+  const myAuctionFactoryProxy = await upgrades.deployProxy(MyAuctionFactory, [getAddr(deployer), getAddr(priceProvider), getAddr(beacon)], {
     kind: "uups",
   });
   console.log("部署完成MyAuctionFactory合约 :", getAddr(myAuctionFactoryProxy));
